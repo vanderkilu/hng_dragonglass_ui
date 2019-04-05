a00818b008169a467551c7f081d8d2264f9edca9
TC Asoluka <austinjerrytech@users.noreply.github.com>
2019-04-05 21:15:29 +0100

    Front End Upload
    Initial front-end upload as requested by front-end team lead Uche Ozeomena

---
+++ hng/includes/head.php
@@ -0,0 +1,19 @@
+<head>
+	<title>HNG Bug Report</title>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+
+	<meta name="description" content="HND Internship 5 Assignments">
+	<meta name="author" content="Austin Asoluka">
+	<link rel="icon" href="images/fiverr.jpg">
+
+	<!-- Google Fonts -->
+	<link href="https://fonts.googleapis.com/css?family=B612:700|Josefin+Sans:300" rel="stylesheet">
+
+	<link rel="stylesheet" type="text/css" href="bootstrap-3.3.7-dist/css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="bootstrap-3.3.7-dist/css/stylesheet.css">
+	<script src="bootstrap-3.3.7-dist/js/jquery-3.2.1.js"></script>
+	<script type="text/javascript" src="bootstrap-3.3.7-dist/js/bootstrap.js"></script>
+</head>
---
+++ hng/index.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html>
+	<?php include'includes/head.php'; ?>
+<body>
+	<div class="parent">
+		<div class="container padd">
+			<div class="row">
+				<div class="col-sm-3">
+					<div class="row">
+						<div class="col-sm-9">
+							<div class="row">
+								<img class="img-responsive profile-pic" src="images/fiverr.jpg" alt="author picture">
+								<div class="row text-center">
+									<h3>TONI ATUNRASE</h3>
+									<h4 class="occupation">Web/Software Developer</h4>
+									<p>Software Enginner at AWS design enthusiast and music lover</p>
+									<i class="fa fa-medium"></i>
+									<i class="fa fa-github"></i>
+									<i class="fa fa-twitter"></i>
+								</div>
+							</div>
+							<hr>
+							<div class="row">
+								<ul class="side_nav">
+								  <li><a class="side_nav" href="#home">Home</a></li>
+								  <li><a class="side_nav" href="#news">News</a></li>
+								  <li><a class="side_nav" href="#contact">Contact</a></li>
+								  <li><a class="side_nav" href="#about">About</a></li>
+								</ul>
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<div class="col-sm-9">
+					<div class="row">
+						<div class="col-sm-12">
+							<div class="col-sm-12 status-area">
+								<input class="form-control" type="text" name="blog_title" placeholder="Title">
+								<textarea class="form-control" placeholder="Tell your story..."></textarea>
+							</div>
+						</div>
+					</div>
+					<hr>
+					<div class="row">
+						<div class="col-sm-12">
+								<div class="row">
+									<div class="col-sm-12">
+										<h1>LATEST STORIES</h1>
+									</div>
+								</div>
+								<div class="row post">
+									<div class="col-sm-2">
+										<img class="img-responsive profile-pic" src="images/fiverr.jpg" alt="author picture">
+									</div>
+									<div class="col-sm-10">
+										<h6>April 10th, 2019</h6>
+										<p>We need to ask whether our technological progress is antagonistic to the place that we call home. was recently revealed that Coca-Cola currently produces three million tonnes of plastic packaging a year which was.....</p>
+									</div>
+								</div>
+
+								<div class="row post">
+									<div class="col-sm-2">
+										<img class="img-responsive profile-pic" src="images/fiverr.jpg" alt="author picture">
+									</div>
+									<div class="col-sm-10">
+										<h6>April 10th, 2019</h6>
+										<p>We need to ask whether our technological progress is antagonistic to the place that we call home. was recently revealed that Coca-Cola currently produces three million tonnes of plastic packaging a year which was.....</p>
+									</div>
+								</div>
+
+								<div class="row post">
+									<div class="col-sm-2">
+										<img class="img-responsive profile-pic" src="images/fiverr.jpg" alt="author picture">
+									</div>
+									<div class="col-sm-10">
+										<h6>April 10th, 2019</h6>
+										<p>We need to ask whether our technological progress is antagonistic to the place that we call home. was recently revealed that Coca-Cola currently produces three million tonnes of plastic packaging a year which was.....</p>
+									</div>
+								</div>
+
+								<div class="row post">
+									<div class="col-sm-2">
+										<img class="img-responsive profile-pic" src="images/fiverr.jpg" alt="author picture">
+									</div>
+									<div class="col-sm-10">
+										<h6>April 10th, 2019</h6>
+										<p>We need to ask whether our technological progress is antagonistic to the place that we call home. was recently revealed that Coca-Cola currently produces three million tonnes of plastic packaging a year which was.....</p>
+									</div>
+								</div>
+							<div class="col-sm-6"></div>
+							<div class="col-sm-6"></div>
+						</div>
+					</div>
+				</div>
+				<div class="col-sm-2"></div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
